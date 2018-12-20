867d9c37e2c700234097e19849391c5b78d35a37
Juan Pablo Pachar Viñan <jpablopachar1993@gmail.com>
2018-12-20 14:33:48 -0500

    Configuración del motor de plantillas

--- src/server/config.js
+++ src/server/config.js
@@ -1,7 +1,30 @@
+const exphbs = require('express-handlebars');
+const path = require('path');
+
+const help = require('./helpers');
+
 module.exports = (app) => {
   /*                  Ajustes                     */
   // Usa el puerto establecido o usa el puerto 3000
   app.set('port', process.env.PORT || 3000);
+
+  /*                   Vistas                      */
+  app.set('views', path.join(__dirname, 'views'));
+  app.engine('.hbs', exphbs({
+    defaultLayout: 'main',
+    layoutsDir: path.join(app.get('views'), 'layouts'),
+    partialsDir: path.join(app.get('views'), 'partials'),
+    extname: '.hbs',
+    // helpers: require('./helpers'),
+    helpers: help,
+  }));
+  app.set('view engine', 'hbs');
+
+  /*                 Middleware                   */
+
+  /*                    Rutas                     */
+
+  /*             Manejador de errores             */
 
   return app;
 };
